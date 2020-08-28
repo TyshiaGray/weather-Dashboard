2a44498c931c4e8e31b766d935480956000dcf83
Tyshia Gray <TyshiaGray@gmail.com>
2020-08-28 16:33:51 -0400

    added local storage, five-day forecast, worked with Tutor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
 var apiKey ="ac1e7dcc53990d9a176a8400fd79a52b"
+var pCities = JSON.parse(localStorage.getItem("weather")) || []
 $("#search").click(function (){
     var citySearch = $("#citySearch").val()
     console.log (citySearch)
     getWeather(citySearch)
+    pCities.unshift(citySearch)
+    localStorage.setItem("weather", JSON.stringify(pCities))
+    renderList()
 })
 function getWeather(citySearch){
+    $(".current").empty()
     $.get(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=${apiKey}`)
     .then(function(results){
         console.log(results)
@@ -21,7 +26,47 @@ function getWeather(citySearch){
             var UV =$("<div>").text("UV index: "+ res.value)
             newDiv.append(title, temperature, humidity, windSpeed, UV)
             $(".current").append(newDiv)
-
+            fiveDay(citySearch)
         })
     })
 }
+function fiveDay(city){
+    $.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`)
+    .then(function(results){
+        console.log(results)
+        $(".future").empty()
+        for (let i = 0; i < results.list.length; i++){
+            let currentSection = results.list[i]
+            
+            if (currentSection.dt_txt.includes("12:00")){
+                var newDiv = $("<div class = 'card col-md-2 bg-primary text-light' style='font-size:.5em'>").text(moment.unix(currentSection.dt).format('L'))
+                var img = $("<img width='50px'>")
+                img.attr('src', `http://openweathermap.org/img/wn/${currentSection.weather[0].icon}.png`)
+                var temperature = $("<div>").text("Temp: "+currentSection.main.temp+ "F")
+                var humidity = $("<div>").text("Humidity: "+currentSection.main.humidity+ "%")
+                newDiv.append(img, temperature, humidity)
+                $(".future").append(newDiv)
+            }
+        }
+
+    })
+}
+
+function renderList(){
+    $("#previousCities").empty()
+    for (var i = 0; i < pCities.length; i++){
+        var divList = $("<div class='card text-center previousCity'>").text(pCities[i])
+        divList.attr("data-name", pCities[i])
+        $("#previousCities").append(divList)
+    }
+}
+$("#previousCities").on("click", ".previousCity", function (){
+    var cityClicked = $(this).attr("data-name")
+    getWeather(cityClicked)
+})
+
+function init(){
+    renderList()
+    getWeather(pCities[0])
+}
+init()
